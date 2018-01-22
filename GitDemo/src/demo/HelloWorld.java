71d52db85d99a5d4cab95042632c0fc18055522c
Nivrutti Chaudhari <nivsoftest@gmail.com>
2018-01-23 03:46:15 +0530

    Hello World - Initial commit

---
+++ GitDemo/src/demo/HelloWorld.java
@@ -0,0 +1,21 @@
+package demo;
+
+/***
+* A simple program using for loop
+*/
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		String s = "Hello";
+		String t = "World";
+		System.out.println(s+" "+t+"!");
+		
+		for (int i = 1; i<=10; i++){
+			System.out.println("Line-"+i);
+		}
+		System.out.println("***End***");
+
+	}
+
+}
